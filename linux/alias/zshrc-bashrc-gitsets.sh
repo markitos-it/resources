c9ce071e6ac1eaeeb35f8180d5c59c1b574e2ee6
Marco Antonio - DevSecOps Kulture. The Way of the Artisan <markitos.es.info@gmail.com>
2026-02-22 12:59:36 +0100

    none important changes. styling

--- linux/alias/zshrc-bashrc-gitsets.sh
+++ linux/alias/zshrc-bashrc-gitsets.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 #:[.'.]:>- ==================================================================================
-#:[.'.]:>- Marco Antonio - markitos devsecops kulture
-#:[.'.]:>- The Way of the Artisan
+#:[.'.]:>- Marco Antonio - The Way of the Artisan
 #:[.'.]:>- markitos.es.info@gmail.com
 #:[.'.]:>- 🌍 https://github.com/orgs/markitos-it/repositories
 #:[.'.]:>- 📺 https://www.youtube.com/@markitos_devsecops
@@ -10,12 +9,12 @@
 
 #:[.'.]:>-==================================================================================
 #:[.'.]:>- Alias para configurar git con un solo comando. Personaliza los valores antes de usarlo.
-#:[.'.]:>- Asegúrate de reemplazar "put your name here", "put your github email here", y "put-private-key" con tus datos reales.
+#:[.'.]:>- Asegúrate de reemplazar "put your name here", "put your github email here", y "github" con tus datos reales.
 #:[.'.]:>- Inserta este alias en tu .zshrc o .bashrc para usarlo en tu terminal.
 #:[.'.]:>-==================================================================================
-alias gitsets='git config user.name "put your name here" && \
-git config user.email "put your github email here" && \
-git config core.sshCommand "ssh -i ~/.ssh/put-private-key -o IdentitiesOnly=yes" && \
+alias gitsets='git config user.name "Marco Antonio - The Way of the Artisan" && \
+git config user.email "markitos.es.info@gmail.com" && \
+git config core.sshCommand "ssh -i ~/.ssh/github -o IdentitiesOnly=yes" && \
 git config core.editor "code --wait" && \
 git config init.defaultBranch main && \
 git config pull.rebase false && \
